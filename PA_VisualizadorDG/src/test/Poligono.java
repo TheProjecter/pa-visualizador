e62819d9d07075c41e196b5ba927414caf03d033
maocleal <maocleal@cd94e1fb-8b68-72a0-6c73-b17f789e6a93>
2011-05-06 23:36:32 +0000

    Classes de testes para Josevan e Lucas treinarem Java e SVN

---
+++ PA_VisualizadorDG/src/test/Poligono.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author nephila
+ */
+abstract class Poligono {
+
+    private int largura;
+    private int altura;
+
+    /*
+     * Método com o intuito de calcular a área do polígono específico
+     */
+    public abstract int calcularArea();
+
+}
---
+++ PA_VisualizadorDG/src/test/Triangulo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author nephila
+ */
+public class Triangulo extends Poligono {
+
+    @Override
+    /*
+     * TODO: Calcular a área do Polígono específico
+     * usar atributos herdados da classe pai Poligono
+     * Responsável: Josevan
+     */
+    public int calcularArea() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
