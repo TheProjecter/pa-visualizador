1e639d19adc694bc01e19aca02ea67277e45d393
maocleal <maocleal@cd94e1fb-8b68-72a0-6c73-b17f789e6a93>
2011-05-06 22:07:45 +0000

    Classe para testes de subversion

---
+++ PA_VisualizadorDG/src/test/Test.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author nephila
+ */
+public class Test {
+
+    private int parametro1;
+    private int parametro2;
+    private int resultado;
+
+    /**
+     * Função Soma
+     * TODO: retornar a soma dos dois parâmetros
+     */
+    public int soma(parametro1,parametro2){        
+    }
+
+    /*
+     * Função Juntar
+     * TODO: retornar um número composto pela junção do primeiro parâmetro
+     * com o segundo
+     */
+    public int (parametro1,parametro2){
+    }
+
+}
