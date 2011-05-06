cc27c48d35334189a99b400b7d7f9cc06ff38c62
maocleal <maocleal@cd94e1fb-8b68-72a0-6c73-b17f789e6a93>
2011-05-06 23:36:32 +0000

    Classes de testes para Josevan e Lucas treinarem Java e SVN

---
+++ trunk/PA_VisualizadorDG/src/test/Quadrado.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author nephila
+ */
+public class Quadrado extends Poligono {
+
+    @Override
+    /*
+     * TODO: Calcular a área do Polígono específico
+     * usar atributos herdados da classe pai Poligono
+     *
+     * Responsável: Lucas
+     */
+    public int calcularArea(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
